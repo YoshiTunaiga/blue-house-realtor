5069fe8682c4c37a2fa376bb56517d2b4b233f8c
Gi Diaz <yandelydf@gmail.com>
2022-02-21 20:16:56 -0500

    (Task): Hamburger menu created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Dropdown from "./Navbar/Dropdown";
 import Navbar from "./Navbar/Navbar";
 
 function App() {
+  const [isOpen, setisOpen] = useState(false);
+
+  const toggle = () => {
+    setisOpen(!isOpen);
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
     </>
   );
 }
---
+++ src/config/colors.js
@@ -0,0 +1,4 @@
+export const colors = {
+  primary: "#0a44dd",
+  white: "#ffffff",
+};
